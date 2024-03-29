13c8df7910c85103e490827d0bf656b165048edd
shirleytang99 <43102406+shirleytang99@users.noreply.github.com>
2024-03-29 12:25:04 +0800

    Week 2-5 class exercises

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write("Hello world!");
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert("Hello " + response + ", have a nice day!");
+};
+//sayHello();
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+};
